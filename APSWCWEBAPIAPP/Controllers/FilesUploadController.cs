7aee8c08e3a06ac927ec7d1034ba4eea13bb0060
DELL <DELL@DESKTOP-3522EN7>
2021-04-20 10:58:59 +0530

    added upload documents method

--- APSWCWEBAPIAPP/Controllers/FilesUploadController.cs
+++ APSWCWEBAPIAPP/Controllers/FilesUploadController.cs
@@ -87,5 +87,38 @@ namespace APSWCWEBAPIAPP.Controllers
                 return StatusCode(500, $"Internal server error: {ex}");
             }
         }
+        [HttpPost, DisableRequestSizeLimit]
+        [Route("WHUploadFileDetails")]
+        public IActionResult WHUploadFileDetails()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("WareHouse", "Documents");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    bool folderExists = Directory.Exists(pathToSave);
+                    if (!folderExists)
+                        Directory.CreateDirectory(pathToSave);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
     }
 }
